149aaf08a2f3a569227ca8852439df5c565cac3d
Francisco Barrios <shighetari@gmail.com>
2020-06-18 13:01:40 -0400

    Change title, remove serverless function

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
-import './App.css';
+import React from "react";
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [date, setDate] = useState(null);
-  useEffect(() => {
-    async function getDate() {
-      const res = await fetch('/api/date');
-      const newDate = await res.text();
-      setDate(newDate);
-    }
-    getDate();
-  }, []);
   return (
     <main>
-      <h1>Create React App + Go API</h1>
+      <h1>Web 31 is _AMAZING_</h1>
       <h2>
-        Deployed with{' '}
+        Deployed with{" "}
         <a
           href="https://vercel.com/docs"
           target="_blank"
@@ -33,15 +24,15 @@ function App() {
           rel="noreferrer noopener"
         >
           This project
-        </a>{' '}
-        was bootstrapped with{' '}
+        </a>{" "}
+        was bootstrapped with{" "}
         <a href="https://facebook.github.io/create-react-app/">
           Create React App
-        </a>{' '}
-        and contains three directories, <code>/public</code> for static assets,{' '}
-        <code>/src</code> for components and content, and <code>/api</code>{' '}
-        which contains a serverless <a href="https://golang.org/">Go</a>{' '}
-        function. See{' '}
+        </a>{" "}
+        and contains three directories, <code>/public</code> for static assets,{" "}
+        <code>/src</code> for components and content, and <code>/api</code>{" "}
+        which contains a serverless <a href="https://golang.org/">Go</a>{" "}
+        function. See{" "}
         <a href="/api/date">
           <code>api/date</code> for the Date API with Go
         </a>
@@ -49,7 +40,6 @@ function App() {
       </p>
       <br />
       <h2>The date according to Go is:</h2>
-      <p>{date ? date : 'Loading date...'}</p>
     </main>
   );
 }
